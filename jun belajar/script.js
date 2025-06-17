4577c8d65feb3c164dd009d53406094535861bf9
Juncak <mzd772@gmail.com>
2025-06-17 09:39:14 +0700

    perbaiki

---
+++ jun belajar/script.js
@@ -0,0 +1,7 @@
+document.getElementById('tombol').onclick = function() {
+    alert('Terima kasih telah mengunjungi situs web kami!, semoga anda puas dengan produk yang kami tawarkan');
+}
+
+document.getElementById('tombol2').onclick = function() {
+    alert('Terima kasih ');
+}
