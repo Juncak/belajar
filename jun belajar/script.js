852d2bfd5b6cc93b0272b12da3b787d962ad3a59
Juncak <mzd772@gmail.com>
2025-06-17 11:10:31 +0700

    Update script.js

--- jun belajar/script.js
+++ jun belajar/script.js
@@ -1,7 +1,6 @@
 document.getElementById('tombol').onclick = function() {
     alert('Terima kasih telah mengunjungi situs web kami!, semoga anda puas dengan produk yang kami tawarkan');
 }
-
 document.getElementById('tombol2').onclick = function() {
-    alert('Terima kasih ');
+    alert('Pastikan Produk yang anda pilih sudah benar');
 }
